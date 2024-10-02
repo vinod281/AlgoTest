73854e06b6797b6d9a1fbfd60819e2903de21bc8
vinod281 <57266038+vinod281@users.noreply.github.com>
2024-10-02 13:53:34 +0530

    Initial commit

---
+++ Algo1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Algo1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string message1 = "Hello";
+            string message2 = "World";
+
+            Console.WriteLine(message1 + message2);
+        }
+
+
+        public static int LinearSearch()
+        {
+
+
+            return 123;
+        }
+    }
+}
